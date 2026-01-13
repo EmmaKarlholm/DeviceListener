42915ce9a0e56cdd026c4a300bca7cf728fd5fa5
EmmaKarlholm <emma.karlholm@gmail.com>
2026-01-13 17:47:01 +0100

    Cleaned up Program.cs

--- DeviceListener/Program.cs
+++ DeviceListener/Program.cs
@@ -9,56 +9,6 @@ namespace DeviceListener
         static void Main(string[] args)
         {
             StartUpManager.Boot(args);
-            //Console.WriteLine("input " + config["input"]);
-            //Console.WriteLine("output " + config["output"]);
-
-
-            //Console.WriteLine($"inputChoice = {inputChoice}");
-            //Console.WriteLine($"outputChocie = {outputChoice}");
-
-            //string inputArg = config["input"]!;
-            //string outputArg = config["output"]!;
-
-
-
-
-
-
-
-            //bool wasSuccessful = int.TryParse(inputArg, out int inputChoice);
-            //int outputChoice = 0;
-            //if (wasSuccessful)
-            //{
-            //    wasSuccessful = int.TryParse(outputArg, out outputChoice);
-            //}
-
-            //if (wasSuccessful)
-            //{
-            //    Console.WriteLine("Both could be parsed into integers");
-            //}
-
-
-            //Console.WriteLine(inputArg);
-            //Console.WriteLine(outputArg);
-
-
-            //inputChoice = Menu.UserChoice(controller.GetInputDeviceNameList());
-            //int userSelection = 0;
-            //if (args.Length == 0)
-            //{
-            //    Console.WriteLine("Please select an audio input device to listen to.\n");
-            //    //userSelection = Menu.UserChoice(controller.GetDeviceNameList());
-            //}
-            //else if (args.Length == 1)
-            //{
-            //    bool wasNumber = int.TryParse(args[0], out int deviceNumber);
-            //    if (wasNumber && deviceNumber < controller.InputDevices.Count)
-            //    {
-            //        userSelection = deviceNumber;
-            //    }
-            //}
-
-            //controller.Listen(userSelection);
         }
     }
 }
