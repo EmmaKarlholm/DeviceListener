65c274a74dfdd6ed545a347e4a45210ed16a0c62
EmmaKarlholm <emma.karlholm@gmail.com>
2026-01-13 17:42:00 +0100

    Code clean-up and improved variable names

--- DeviceListener/StartUpManager.cs
+++ DeviceListener/StartUpManager.cs
@@ -19,51 +19,32 @@ namespace DeviceListener
                 .AddCommandLine(args)
                 .Build();
 
-
-
-
-
-            //if  ((!int.TryParse(config["output"], out int outputChoice))
-            //    || !int.TryParse(config["input"], out int inputChoice))
-            //{
-            //    Console.WriteLine("At least one of them could not be parsed, will handle as if no input");
-            //}
-
             int inputChoice;
             int outputChoice;
 
-            bool inputArgValid = int.TryParse(config["input"], out inputChoice);
-            bool outputArgValid = int.TryParse(config["output"], out outputChoice);
+            bool inputArgIsInteger = int.TryParse(config["input"], out inputChoice);
+            bool outputArgIsInteger = int.TryParse(config["output"], out outputChoice);
 
-
-
-
-
-
-            if (inputArgValid
-                && outputArgValid
-                && inputChoice < controller.InputDevices.Count
-                && outputChoice < controller.InputDevices.Count)
+            if (!inputArgIsInteger 
+                || inputChoice < 0 
+                || inputChoice >= controller.InputDevices.Count)
             {
-                controller.Listen(inputChoice, outputChoice);
+            Menu.DrawHeader("Please select an input device. (the device being recorded)");
+            inputChoice = Menu.UserChoice(controller.GetDeviceNameList(controller.InputDevices));                
             }
 
-
-
-            Menu.DrawHeader("Please select an input device. (the device being recorded)");
-            inputChoice = Menu.UserChoice(controller.GetDeviceNameList(controller.InputDevices));
+            if (!outputArgIsInteger
+                || outputChoice < 0
+                || outputChoice >= controller.OutputDevices.Count)
+            {
             Menu.DrawHeader("Please select an output device (the device where you listen)");
             outputChoice = Menu.UserChoice(controller.GetDeviceNameList(controller.OutputDevices));
+            }
+
+            controller.Listen(inputChoice, outputChoice);
 
             Console.WriteLine($"inputChoice is {inputChoice}");
             Console.WriteLine($"outputChoice is {outputChoice}");
-
-
-            Console.ReadKey(true);
-            Console.ReadKey(true);
-            Console.ReadKey(true);
-            Console.ReadKey(true);
-
         }
     }
 }
